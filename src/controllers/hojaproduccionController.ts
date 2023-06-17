6698498c54c12492eefda0e3004f77d0a46f9b37
Gonzalo Manuel Cuevas Hinojosa <manuelhinojosagc@gmail.com>
2023-06-17 12:41:45 -0400

    br F

--- src/controllers/hojaproduccionController.ts
+++ src/controllers/hojaproduccionController.ts
@@ -19,7 +19,7 @@ class HojaProdController {
         res.json(hojaprod)
     }
     public async hoyrec(req: Request, res: Response) {
-        const [hojaprod] = await pool.promise().query('SELECT hoja_de_produccion.id_hoja_produccion,producto.nombre AS receta,hoja_de_produccion.cantidad, hoja_de_produccion.fecha_hoja,hoja_de_produccion.encargado,hoja_de_produccion.progreso,hoja_de_produccion.peso_recibido, hoja_de_produccion.embolsado, cantidad_bolsa,peso_receta,(SELECT COUNT(*) FROM hoja_de_produccion WHERE STR_TO_DATE(fecha_hoja, "%Y-%m-%d") = CURDATE()) AS recetas_hoy FROM hoja_de_produccion INNER JOIN receta ON hoja_de_produccion.id_receta = receta.id_receta INNER JOIN producto ON receta.id_producto = producto.id_producto WHERE STR_TO_DATE(fecha_hoja, "%Y-%m-%d") = CURDATE() AND progreso >=20');
+        const [hojaprod] = await pool.promise().query('SELECT hoja_de_produccion.id_hoja_produccion, producto.nombre AS receta, hoja_de_produccion.cantidad, hoja_de_produccion.fecha_hoja, hoja_de_produccion.encargado, hoja_de_produccion.progreso, hoja_de_produccion.peso_recibido, hoja_de_produccion.embolsado, cantidad_bolsa, peso_receta, (SELECT COUNT(*) FROM hoja_de_produccion WHERE STR_TO_DATE(fecha_hoja, "%Y-%m-%d") = CURDATE()) AS recetas_hoy, ROUND((embolsado / cantidad_bolsa) * 100, 2) AS porcentaje_embolsado, ROUND((peso_recibido / peso_receta) * 100, 2) AS porcentaje_peso_recibido FROM hoja_de_produccion INNER JOIN receta ON hoja_de_produccion.id_receta = receta.id_receta INNER JOIN producto ON receta.id_producto = producto.id_producto WHERE STR_TO_DATE(fecha_hoja, "%Y-%m-%d") = CURDATE() AND progreso >= 20;');
         res.json(hojaprod)
     }
 
